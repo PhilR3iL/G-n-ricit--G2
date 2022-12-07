5d2a815550068676756d9d0675d91c47b1da05dd
Philippe ROUSSILLE <117383554+PhilR3iL@users.noreply.github.com>
2022-12-07 14:52:46 +0000

    Exo 3.2

--- exo31/Exo1.java
+++ exo31/Exo1.java
@@ -1,9 +1,14 @@
 package exo31;
 
-public static class Exo1{
+public class Exo1{
 
   public static <T> T alea(T[] tableau){
     return tableau[(int)(Math.random() * (tableau.length - 1))];
   }
+
+  public static <T> alea(T un, T deux){
+    return Math.random() > 0.5? un: deux;
+  }
+
   
 }
