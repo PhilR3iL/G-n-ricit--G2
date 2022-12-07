af5e644e6ae9e508c1b7f447351b731606fd0b57
Philippe ROUSSILLE <117383554+PhilR3iL@users.noreply.github.com>
2022-12-07 15:08:35 +0000

    exo 4.1)a)

---
+++ exo41/CoupleNomme.java
@@ -0,0 +1,14 @@
+package exo41;
+
+public class CoupleNomme<T> extends Couple<T>{
+  private String nom;
+  public CoupleNomme(T premier, T second, String nom){
+    super(premier, second)  ;
+    this.nom = nom;
+  }
+  @Override
+  public void afficher(){
+    System.out.println("nom : " + nom);
+    super.afficher();
+  }
+}
